24dbfa90f8f4ca4db4977805ef39568cf866ebba
AndreyPC <andreypc@Andreys-MacBook-Pro.local>
2022-04-17 18:14:12 +0300

    feat: Add burger menu

--- script/modal.js
+++ script/modal.js
@@ -1,6 +1,11 @@
 let burgerMenu = document.querySelector('.burger__menu');
-let listMenu = document.querySelector('.list__settings')
+let listMenu = document.querySelector('.list__settings');
+let burgerMenuExit = document.querySelector('.cancel');
 const openMenu = () => {
     burgerMenu.style.display = "none";
-    listMenu.style.display = "block";
+    listMenu.style.display = "flex";
+}
+const closeMenu = () => {
+    listMenu.style.display = "none";
+    burgerMenu.style.display = "block";
 }
