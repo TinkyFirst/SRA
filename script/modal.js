bc4babe3ecaa6ea403669bdd8e28101b4e77710c
AndreyPC <andreypc@Andreys-MacBook-Pro.local>
2022-04-17 15:29:01 +0300

    feat: create burger menu

---
+++ script/modal.js
@@ -0,0 +1,6 @@
+let burgerMenu = document.querySelector('.burger__menu');
+let listMenu = document.querySelector('.list__settings')
+const openMenu = () => {
+    burgerMenu.style.display = "none";
+    listMenu.style.display = "block";
+}
