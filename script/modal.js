ef330c6b0ecaac5d4d77ff1f8e341a75e8a9faa4
AndreyPC <andreypc@Andreys-MacBook-Pro.local>
2022-05-09 16:29:22 +0300

    add theme button

--- script/modal.js
+++ script/modal.js
@@ -10,41 +10,106 @@ let settingsBlock = document.querySelector('.settigs_block')
 let settingButton = document.querySelector('.svg_setting')
 let downloadBlock = document.querySelector('.download__block')
 let downloadButton = document.querySelector('.download')
+let themeButton = document.querySelector('.change__theme')
+let settingBlock = document.querySelector('.settigs_block')
+let htmlPage = document.querySelector('html')
+let settingsHeading = document.querySelector('.settings_heading')
+let burgerSvg = document.querySelector('.burger_svg')
+let buttonBtn = document.querySelector('.button')
+let downloadBtn = document.querySelector('.download')
+let containerStats = document.querySelector('.container')
+let cancelSvg = document.querySelector('.svg_close')
+let downloadHeading = document.querySelector('.download__heading')
+let modalBlock = document.querySelector('.modal-content')
+
+const currentTheme = localStorage.getItem("theme");
+
 
 const openMenu = () => {
-    burgerMenu.style.display = "none";
-    listMenu.style.display = "flex";
+  burgerMenu.style.display = "none";
+  listMenu.style.display = "flex";
 }
 const closeMenu = () => {
-    listMenu.style.display = "none";
-    burgerMenu.style.display = "block";
-    displayContent.style.display = "block";
-    bannedSitesContent.style.display = "none";
-    statsBlock.style.display = "flex";
-    appHeading.style.display = "block";
+  listMenu.style.display = "none";
+  burgerMenu.style.display = "block";
+  displayContent.style.display = "block";
+  bannedSitesContent.style.display = "none";
+  statsBlock.style.display = "flex";
+  appHeading.style.display = "block";
+  settingsBlock.style.display = "none";
 
 }
 const openBannedSited = () => {
-    statsBlock.style.display = "none";
-    appHeading.style.display = "none";
-    bannedSitesContent.style.display = "block";
+  statsBlock.style.display = "none";
+  appHeading.style.display = "none";
+  bannedSitesContent.style.display = "block";
 }
 const openSettings = () => {
-    statsBlock.style.display = "none";
-    appHeading.style.display = "none";
-    bannedSitesContent.style.display = "none";
-    settingsBlock.style.display = "block";
-    listMenu.style.display = "none";
-    burgerMenu.style.display = 'block'
+  statsBlock.style.display = "none";
+  appHeading.style.display = "none";
+  bannedSitesContent.style.display = "none";
+  settingsBlock.style.display = "block";
+  listMenu.style.display = "none";
+  burgerMenu.style.display = 'block'
 }
 const openDownload = () => {
-    statsBlock.style.display = "none";
-    appHeading.style.display = "none";
-    bannedSitesContent.style.display = "none";
-    settingsBlock.style.display = "none";
-    downloadBlock.style.display = "block"
-    listMenu.style.display = "none";
-    burgerMenu.style.display = 'block'
+  statsBlock.style.display = "none";
+  appHeading.style.display = "none";
+  bannedSitesContent.style.display = "none";
+  settingsBlock.style.display = "none";
+  downloadBlock.style.display = "block"
+  listMenu.style.display = "none";
+  burgerMenu.style.display = 'block'
 }
 settingButton.addEventListener('click', openSettings)
 downloadButton.addEventListener('click', openDownload)
+
+
+
+if (currentTheme == "dark") {
+  document.body.classList.add("black");
+  htmlPage.classList.add("black");
+  displayContent.classList.add("black");
+  settingsBlock.classList.add("black");
+  settingsHeading.classList.add("black");
+  themeButton.classList.add("button_black")
+  burgerSvg.classList.add("black");
+  appHeading.classList.add("black")
+  statsBlock.classList.add("black")
+  bannedSitesBtn.classList.add("black")
+  downloadBtn.classList.add("black")
+  containerStats.classList.add("black")
+  settingButton.classList.add("svg_black")
+  settingsBlock.classList.add("black")
+  cancelSvg.classList.add("svg_black")
+  modalBlock.classList.add("black")
+}
+
+themeButton.addEventListener('click', function () {
+  document.body.classList.toggle("black");
+  htmlPage.classList.toggle("black");
+  displayContent.classList.toggle("app_black");
+  settingsBlock.classList.toggle("black");
+  settingsHeading.classList.toggle("black");
+  themeButton.classList.toggle("button_black");
+  burgerSvg.classList.toggle("black");
+  appHeading.classList.toggle("black");
+  statsBlock.classList.toggle("black");
+  bannedSitesBtn.classList.toggle("black")
+  downloadBtn.classList.toggle("black")
+  containerStats.classList.toggle("black")
+  downloadHeading.classList.toggle("black")
+  settingButton.classList.toggle("svg_black")
+  cancelSvg.classList.toggle("svg_black")
+  modalBlock.classList.toggle("black")
+
+  // Допустим, тема светлая
+  let theme = "light";
+  // Если <body> содержит класс .dark-theme…
+  if (document.body.classList.contains("black")) {
+    // …тогда делаем тему тёмной
+    theme = "dark";
+  }
+  // После чего сохраняем выбор в localStorage
+  localStorage.setItem("theme", theme);
+})
